0b6b9bee46fa6da3abdaa9afa273d1cd46a36a0d
Konstantin Savkiv <konstantin.savkiv@gmail.com>
2022-10-03 01:39:57 +0300

    recreate migration with book entity

--- migration/src/m20220930_101850_create_book_table.rs
+++ migration/src/m20220930_101850_create_book_table.rs
@@ -9,17 +9,21 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Book::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(Book::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(Book::Title).string().not_null())
+                    .col(ColumnDef::new(Book::Author).string().not_null())
+                    .col(ColumnDef::new(Book::Price).string())
+                    .col(ColumnDef::new(Book::PublishDate).date_time())
+                    .col(ColumnDef::new(Book::CreatedAt).date_time().not_null())
+                    .col(ColumnDef::new(Book::UpdatedAt).date_time().not_null())
                     .to_owned(),
             )
             .await
@@ -27,16 +31,20 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Book::Table).to_owned())
             .await
     }
 }
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Post {
+enum Book {
     Table,
     Id,
     Title,
-    Text,
+    Author,
+    PublishDate,
+    Price,
+    CreatedAt,
+    UpdatedAt
 }
